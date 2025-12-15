c40bb90a88cec16807896954e8a5de10a5f5b0e0
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2025-12-15 16:09:17 +0000

    Sync LeetCode submission Runtime - 20 ms (23.14%), Memory - 14.3 MB (21.44%)

--- my-code/0003-longest-substring-without-repeating-characters/solution.cpp
+++ my-code/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -1,26 +1,36 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-    string temp = "";
-    int maxLen = 0;
-
-    for (int i = 0; i < s.length(); i++) {
-        // Check if s[i] is already in temp
-        for (int j = 0; j < temp.length(); j++) {
-            if (temp[j] == s[i]) {
-                // Remove everything up to the duplicate
-                temp = temp.substr(j + 1);
-                break;
+        if(s.empty())
+        {
+            return 0;
+        }
+        unordered_map<int,int> m;
+        int currlen=0;
+        int maxlen=1;
+        int p1=0;
+        int p2=0;
+        while(p2<s.size())
+        {
+            if(!m.contains(s[p2]))
+            {
+            m[s[p2]]=1;
+            p2++;
+            currlen=p2-p1;
+            maxlen=max(maxlen,currlen);
+            }
+            else
+            {
+              while(m.contains(s[p2]))
+              {
+                m.erase(s[p1]);
+                p1++;
+              }
+              m[s[p2]]=1;
+              p2++;
+              maxlen=max(maxlen,currlen);
             }
         }
-
-        temp += s[i]; // Add current character
-        if (temp.length() > maxLen) {
-            maxLen = temp.length();
-        }
+        return maxlen;
     }
-
-    return maxLen;
-}
-
 };
