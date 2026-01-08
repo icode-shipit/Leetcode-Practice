10ef5ec37611db2dfe07b4f7bb94290c279779ca
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-01-08 16:38:47 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 22.5 MB (55.18%)

--- my-code/0337-house-robber-iii/solution.cpp
+++ my-code/0337-house-robber-iii/solution.cpp
@@ -11,19 +11,19 @@
  */
 class Solution {
 public:
-   pair<int,int> dfs(TreeNode* root) // first->withroot maxmoney ,second->withroot maxmoney
-   {
-    if(root==NULL)
+
+    pair<int,int> dfs(TreeNode* root) //first->withroot max,second->withoutroot max
     {
-      return {0,0};
+        if(root==NULL)
+        {
+            return {0,0};
+        }
+        pair<int,int> leftpair= dfs(root->left);
+        pair<int,int> rightpair= dfs(root->right);
+        int left=root->val+leftpair.second+rightpair.second;
+        int right=max(leftpair.first,leftpair.second)+max(rightpair.first,rightpair.second);
+        return {left,right};
     }
-    pair<int,int> leftpair=dfs(root->left);
-    pair<int,int> rightpair=dfs(root->right);
-    
-    int withroot=root->val+leftpair.second+rightpair.second;
-    int withoutroot=max(leftpair.first,leftpair.second)+max(rightpair.first,rightpair.second);
-    return {withroot,withoutroot};  
-   }
 
     int rob(TreeNode* root) {
         pair<int,int> ans=dfs(root);
