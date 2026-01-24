01ca6bf57d95bbdbf7b8d66b62adf5a744d9cb64
Tejesh Sakhamuri <thanmaisakhamuri999@gmail.com>
2026-01-24 12:02:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9 MB (87.49%)

---
+++ my-code/1993-sum-of-all-subset-xor-totals/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int ans;
+    void backtracking(vector<int>& nums,int p,int curr_xor)
+    {
+        if(p==nums.size())
+        {
+            ans=ans+curr_xor;
+            return;
+        }
+        backtracking(nums,p+1,curr_xor); // not include case
+        curr_xor= curr_xor ^ nums[p];
+        backtracking(nums,p+1,curr_xor); // include case
+        return;
+    }
+    int subsetXORSum(vector<int>& nums) {
+        backtracking(nums,0,0);
+        return ans;
+    }
+};
